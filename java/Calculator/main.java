efecf9ed51f1dd5d7cd90537a789d5cfca9faf8f
urmoter <97073131+urmoter@users.noreply.github.com>
2022-09-27 19:29:09 -0400

    fixing VSCode nagging me.

--- java/Calculator/main.java
+++ java/Calculator/main.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
 public class main{
-    public static void main(String[] args) {
+    public static void abc(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("What's your name?: ");
         String name = scan.nextLine();
         System.out.print("hello " + name + "!");
+        scan.close();
         return;
     }
 }
